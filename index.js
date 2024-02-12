dd97516dfa9f1375f9bcfba274e182429bf49799
SarvsDev <158330686+SarvsDev@users.noreply.github.com>
2024-02-12 12:48:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const channelId = '';
 
 async function login() {
   try {
-    await client.login("MTExODUzMTkxNTAwODQ1MDY3NA.G7fEJR.PkNbZlWxLxLZZG-cWx2iQ5N7Co89n4lVCheyWE");
+    await client.login(process.env.TOKEN);
     console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
   } catch (error) {
     console.error('Failed to log in:', error);
